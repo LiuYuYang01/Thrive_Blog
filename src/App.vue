17fecf40cd17fa479f0044b72877d1b0c4d6fd5d
刘宇阳 <3311118881@qq.com>
2023-04-09 20:47:13 +0800

    注释

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!-- 
+Author：刘宇阳,
+Blog：http://liuyuyang.net/
+Time：2023年04月09日
+GitHub：https://github.com/LiuYuYang01
+Info：互联网从不缺乏天才，而努力才是最终的入场券！
+-->
+
 <script setup lang='ts'>
 </script>
 
@@ -6,5 +14,4 @@
 </template>
 
 <style scoped lang="scss">
-
 </style>
