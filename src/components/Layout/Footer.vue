f1e213a4b9b287a9c6aba967f570742ebbd303bd
刘宇阳 <3311118881@qq.com>
2023-05-05 20:35:52 +0800

    完成页面底部功能

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -3,9 +3,24 @@
 </script>
 
 <template>
-  <div>底部</div>
+  <div class="footer">
+    <p class="info">一直对网站开发领域很感兴趣，从小就希望有一个属于自己的网站，在17年时候成功进入站长圈，并通过各种自学，以及各种折腾，才有了你现在看到的这个网站</p>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.footer {
+  display: flex;
+  align-items: center;
+  height: 120px;
+  border: 1px solid #f0f0f0;
+  background-color: #fff;
 
+  .info {
+    width: 600px;
+    padding-left: 300px;
+    color: #333;
+    font-size: 14px;
+  }
+}
 </style>
