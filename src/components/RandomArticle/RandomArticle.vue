243a19ba8f05209a74b57a4587732554b5c22b8e
刘宇阳 <3311118881@qq.com>
2023-05-05 21:27:13 +0800

    完成随机文章布局60%

--- src/components/RandomArticle/RandomArticle.vue
+++ src/components/RandomArticle/RandomArticle.vue
@@ -3,8 +3,72 @@
 </script>
 
 <template>
-  <div>Hello</div>
+    <div class="RandomArticle">
+        <div class="title">
+            随机文章
+        </div>
+
+        <!-- 文章列表 -->
+        <div class="list">
+            <div class="item">
+                <RouterLink to="path">
+                    <iconpark-icon name="right" />
+                    2022年的 React 生态
+                </RouterLink>
+            </div>
+
+            <div class="item">
+                <RouterLink to="path">
+                    <iconpark-icon name="right" />
+                    2022年的 React 生态
+                </RouterLink>
+            </div>
+
+            <div class="item">
+                <RouterLink to="path">
+                    <iconpark-icon name="right" />
+                    2022年的 React 生态
+                </RouterLink>
+            </div>
+
+            <div class="item">
+                <RouterLink to="path">
+                    <iconpark-icon name="right" />
+                    2022年的 React 生态
+                </RouterLink>
+            </div>
+            
+            <div class="item">
+                <RouterLink to="path">
+                    <iconpark-icon name="right" />
+                    2022年的 React 生态
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
+@import "@/styles/public.scss";
+
+.RandomArticle {
+    @include container;
+    padding: 15px;
+    margin-top: 20px;
+    background-color: #fff;
+
+    // 标题
+    .title {
+        font-size: 20px;
+    }
+
+    // 文章列表
+    .list {
+        .item {
+            line-height: 40px;
+            color: #666;
+            font-size: 14px;
+        }
+    }
+}
 </style>
