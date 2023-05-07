6be6486a6acf11bfc5a0ebc0a42239e1851dff9b
刘宇阳 <3311118881@qq.com>
2023-05-07 19:21:43 +0800

    完成随机文章模块布局

--- src/components/RandomArticle/RandomArticle.vue
+++ src/components/RandomArticle/RandomArticle.vue
@@ -5,7 +5,8 @@
 <template>
     <div class="RandomArticle">
         <div class="title">
-            随机文章
+            <img src="@/assets/svg/other/article.svg" alt="">
+            <span> 随机文章</span>
         </div>
 
         <!-- 文章列表 -->
@@ -37,7 +38,7 @@
                     2022年的 React 生态
                 </RouterLink>
             </div>
-            
+
             <div class="item">
                 <RouterLink to="path">
                     <iconpark-icon name="right" />
@@ -57,17 +58,53 @@
     margin-top: 20px;
     background-color: #fff;
 
-    // 标题
+    // 文章模块标题
     .title {
-        font-size: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-bottom: 10px;
+        border-bottom: 1px solid #eee;
+        font-size: 17px;
+        font-weight: 400;
+        color: #333;
+
+        img {
+            width: 30px;
+            height: 20px;
+        }
     }
 
     // 文章列表
     .list {
         .item {
-            line-height: 40px;
-            color: #666;
-            font-size: 14px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+
+            a {
+                display: flex;
+                align-items: center;
+                padding: 10px 0;
+                color: #666;
+                font-size: 14px;
+                transition: padding $move;
+
+                iconpark-icon {
+                    font-size: 18px;
+                    padding-right: 3px;
+                }
+            }
+
+            &:hover a{
+                color: $color;
+                padding-left: 10px;
+                transition: all $move;
+            }
+
+            &:hover iconpark-icon{
+                padding-right: 5px;
+            }
         }
     }
 }
