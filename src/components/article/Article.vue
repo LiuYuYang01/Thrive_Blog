7aba7aa0f44b92e6d4912b37925e2826ad53bdfe
刘宇阳 <3311118881@qq.com>
2023-05-12 15:11:12 +0800

    完成文章标签布局

--- src/components/article/Article.vue
+++ src/components/article/Article.vue
@@ -102,11 +102,24 @@ function createDirectory() {
                     <iconpark-icon name="comment" /> 评论：15
                 </span>
             </div>
-
         </div>
 
         <!-- 文章内容 -->
         <div class="content" v-html="content"></div>
+
+        <!-- 文章标签 -->
+        <div class="tag">
+            <img src="@/assets/svg/other/tag.svg" alt="">
+
+            <!-- 标签列表 -->
+            <div class="list">
+                <a href="javascript:;">Golang</a>
+                <a href="javascript:;">Buffer</a>
+                <a href="javascript:;">开发笔记</a>
+                <a href="javascript:;">生活随笔</a>
+                <a href="javascript:;">学习</a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -162,7 +175,6 @@ function createDirectory() {
     .title {
         padding: 20px 0;
         margin-bottom: 20px;
-        color: #333;
         border-bottom: 1px solid #f3f3f3;
         font-weight: 700;
         font-family: "";
@@ -204,6 +216,9 @@ function createDirectory() {
 
     // 文章内容
     .content {
+        font-size: 15px;
+        font-family: "";
+        color: #333;
 
         // 设置代码框样式
         :deep(pre) {
@@ -238,11 +253,63 @@ function createDirectory() {
             margin: 20px 0 10px;
             // a标签锚点跳转距离顶部的距离
             scroll-margin-top: 80px;
+            color: #000;
         }
 
         :deep(h1) {
             margin-bottom: 20px;
             font-size: 28px;
+        }
+    }
+
+    // 文章标签
+    .tag {
+        display: flex;
+        align-items: center;
+
+        img {
+            width: 20px;
+            height: 20px;
+            margin-right: 5px;
+        }
+
+        // 标签列表
+        .list {
+            font-size: 12px;
+            font-family: "";
+
+            a {
+                padding: 2px 6px;
+                margin: 0 5px;
+                border-radius: 20px;
+                color: #666;
+                background-color: #faf9f9;
+
+                &:nth-child(1) {
+                    color: #0d6efd;
+                    background-color: rgba(13,110,253,.1);
+                }
+
+                &:nth-child(2) {
+                    color: #6610f2;
+                    background-color: rgba(102,16,242,.1);
+                }
+
+                &:nth-child(3) {
+                    color: #20c997;
+                    background-color: rgba(32,201,151,.1);
+                }
+
+                &:nth-child(4) {
+                    color: #dc3545;
+                    background-color: rgba(220,53,69,.1);
+                }
+
+                &:nth-child(5) {
+                    color: #fd7e14;
+                    background-color: rgba(253,126,20,.1);
+                }
+            }
         }
     }
 }
