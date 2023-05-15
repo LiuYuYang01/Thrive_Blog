daed39908ee23ab6b381aa7c6ccd471b7ecf1b9d
刘宇阳 <3311118881@qq.com>
2023-05-15 10:47:48 +0800

    约束作者信息响应类型

--- src/api/User.ts
+++ src/api/User.ts
@@ -3,7 +3,7 @@ import type { Response } from '@/types/Response'
 import type { Author } from '@/types/User'
 
 // 获取作者信息
-export function getAuthorAPI() {
+export function getAuthorAPI(): Promise<Response<Author>> {
     return Request.instance.request({
         url: "/api/author"
     })
