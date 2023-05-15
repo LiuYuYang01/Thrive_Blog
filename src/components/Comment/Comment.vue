cb9d9b7144238758a407864c0d8b23880c2e600e
刘宇阳 <3311118881@qq.com>
2023-05-15 20:43:31 +0800

    完成评论框布局

--- src/components/Comment/Comment.vue
+++ src/components/Comment/Comment.vue
@@ -4,17 +4,43 @@
 
 <template>
     <div class="Comment">
+        <div class="title"></div>
         <textarea name="" id="" cols="30" rows="10"></textarea>
     </div>
 </template>
 
 <style scoped lang="scss">
 .Comment {
-    margin-top: 20px;
+    margin-top: 100px;
+
+    .title {
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background-color: #f7f7f7;
+
+        &::after {
+            content: "评论区";
+            position: absolute;
+            top: -30px;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 180px;
+            background-color: #fff;
+            text-align: center;
+            font-size: 24px;
+        }
+    }
 
     textarea {
         width: 100%;
-        border: none;
+        padding: 20px;
+        margin-top: 40px;
+        border: 1px solid #eee;
+        border-radius: 5px;
+        box-sizing: border-box;
         outline: none;
     }
 }
