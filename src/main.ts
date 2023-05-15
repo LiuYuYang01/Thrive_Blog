82351a0848282cd0aa89b98a7550f984f8464775
刘宇阳 <3311118881@qq.com>
2023-05-15 10:51:39 +0800

    引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import {createPinia} from 'pinia'
 
 import '@/styles/public.scss'
 
@@ -8,5 +9,6 @@ import router from '@/router'
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
