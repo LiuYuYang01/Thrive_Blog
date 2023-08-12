4298efc7fe600ca779e02d2f8dc84bde0ba00098
刘宇阳 <3311118881@qq.com>
2023-08-12 13:43:31 +0800

    封装全屏loading加载效果

--- src/components/Loading/index.vue
+++ src/components/Loading/index.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
-
+const props = defineProps<{ loading: boolean }>()
 </script>
 
 <template>
-  <div class="Loading">
-    <img src="@/assets/img/loading.gif" alt="">
+  <div class="Loading" v-if="loading">
+    <img src="@/assets/svg/other/loading.svg" alt="">
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "@/styles/public.scss";
-
 .Loading {
-  background: #fff;
-  text-align: center;
-  @include container;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  img {
-    width: 100px;
-    height: 100px;
-  }
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  z-index: 9999;
 }
 </style>
--- src/view/Home/index.vue
+++ src/view/Home/index.vue
@@ -2,12 +2,19 @@
 import { Article } from '@/types/Article'
 import { getArticleListAPI } from '@/api/Article'
 
+// 全屏加载效果
+const isLoading = ref<boolean>(false)
+
 const articleList = ref<Article[]>([])
 
 // 获取文章列表
 const getArticleData = async () => {
+  isLoading.value = true
+
   const { data } = await getArticleListAPI()
   articleList.value = data
+
+  isLoading.value = false
 }
 getArticleData()
 </script>
@@ -16,6 +23,8 @@ getArticleData()
   <Swiper src="http://static.liuyuyang.net/images/t01d6f5fd3610da0a08.jpg"></Swiper>
 
   <Frame :modules='["Author", "HotArticle", "RandomArticle", "NewComments"]'>
+    <loading :loading="isLoading"/>
+    
     <Classics :data="articleList" />
   </Frame>
 </template>
