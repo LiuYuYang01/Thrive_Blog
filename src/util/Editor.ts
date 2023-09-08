0c3ee8a5d2b07bfdd20febdd94ba06c75dd8e4cb
刘宇阳 <3311118881@qq.com>
2023-09-08 18:37:56 +0800

    1

--- src/util/Editor.ts
+++ src/util/Editor.ts
@@ -1,7 +1,9 @@
+// @ts-ignore
 import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 
 // GitHub风格
+// @ts-ignore
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 
