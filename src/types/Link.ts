9f4b4f15d03f0d67c9338254bb3fa54dd9b2b077
刘宇阳 <3311118881@qq.com>
2023-10-25 17:23:19 +0800

    完成动态渲染网站列表数据功能

--- src/types/Link.ts
+++ src/types/Link.ts
@@ -1,13 +1,15 @@
 export interface Link {
-    id?: number,
-    title: string,
-    description: string,
-    email: string,
-    image: string,
-    url: string
+  id?: number;
+  title: string;
+  description: string;
+  email: string;
+  image: string;
+  url: string;
+  type: string;
+  date?: string;
 }
 
 export interface Cate {
-    title: string,
-    list: Link[]
+  title: string;
+  list: Link[];
 }
