ef85f959df29fe058aa9d2cc28d819973703facc
刘宇阳 <3311118881@qq.com>
2023-10-25 17:11:05 +0800

    封装空组件

---
+++ src/components/Null/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="null">
+        <img src="@/assets/svg/other/null.svg" alt="">
+        <p>空空如也</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.null {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+        width: 200px;
+        height: 200px;
+    }
+}
+</style>
