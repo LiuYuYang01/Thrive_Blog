94e26a2e8905220509ed9cb9dfb2d90392984694
刘宇阳 <3311118881@qq.com>
2023-11-24 15:30:31 +0800

    完成评论分页效果

--- src/api/Comment.ts
+++ src/api/Comment.ts
@@ -1,25 +1,32 @@
-import Request from '@/util/Request'
+import Request from "@/util/Request";
 
 // 新增评论
 export function addCommentDataAPI(data: Comment): http<Comment> {
-    return Request.instance.request({
-        url: `/api/comment`,
-        method: "post",
-        data
-    })
+  return Request.instance.request({
+    url: `/api/comment`,
+    method: "post",
+    data,
+  });
 }
 
 // 获取评论信息
 export function getCommentDataAPI(id: string): http<Comment[]> {
-    return Request.instance.request({
-        url: `/api/comment/${id}`,
-        params: id
-    })
+  return Request.instance.request({
+    url: `/api/comment/${id}`,
+    params: id,
+  });
 }
 
 // 获取评论列表
-export function getCommentListAPI(): http<Comment[]> {
+export const getCommentListAPI = (params?: Page): http<Comment[]> => {
+  // 如果有参数就是分页查询，没有参数就是查询全部
+  if (params) {
     return Request.instance.request({
-        url: "/api/comment"
-    })
-}
+      url: `/api/comment?page=${params.page}&size=${params.size}`,
+    });
+  } else {
+    return Request.instance.request({
+      url: "/api/comment",
+    });
+  }
+};
