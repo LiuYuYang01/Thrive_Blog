45b7f108110e6242ae11d00c6c29c5e6d84035a5
LiuYuYang01 <3311118881@qq.com>
2024-02-07 21:43:10 +0800

    调整黑夜模式适配效果

--- src/view/Friend/index.vue
+++ src/view/Friend/index.vue
@@ -1,35 +1,47 @@
 <script setup lang="ts">
-import { getLinkListAPI } from '@/api/Link'
-import { Type, Cate } from '@/types/Link'
+import { addLinkDataAPI, getLinkListAPI, getLinkTypeListAPI } from '@/api/Link'
+import { Type, Cate, Link, LinkType } from '@/types/Link'
 import { svg } from '@/util'
-
-// 网站列表数据
-const linkData = reactive<Type>({})
+import { FormInstance } from 'element-plus';
 
 // 加载效果
 const loading = ref(false)
 
-// 获取网站列表数据
+// 网站列表
+const list = ref<Type>({})
+// 网站类型列表
+const linkTypeList = ref<LinkType[]>([])
+
+// 获取网站列表
 const getLinkList = async () => {
     loading.value = true
 
-    const { data } = await getLinkListAPI()
+    list.value = {}
+    const { data } = await getLinkListAPI({ page: 1, size: 99999 })
 
     // 将所有type类型做一个分类
     data.result.forEach(item => {
-        if (linkData[item.type]) {
+        const k = list.value[item.type]
+
+        if (k) {
             // 如果有这个类型就添加数据
-            linkData[item.type].list.push(item)
-            linkData[item.type].type = item.type
+            k.list.push(item)
+            k.type = item.type
         } else {
-            // linkData.value = {}
             // 没有就设置为空数组
-            linkData[item.type] = { type: "", list: [] } as Cate
+            list.value[item.type] = { type: item.type, list: [item] } as Cate
         }
     })
 
     loading.value = false
 }
+
+// 获取网站类型列表
+const getLinkTypeList = async () => {
+    const { data } = await getLinkTypeListAPI()
+    linkTypeList.value = data
+}
+getLinkTypeList()
 
 // 初次加载做一些事情
 onMounted(async () => {
@@ -43,39 +55,94 @@ onMounted(async () => {
 })
 
 // 控制弹框显示与隐藏
-const dialogVisible = ref<boolean>(false)
+const model = ref<boolean>(false)
 
-const linkForm = ref({
+const form = ref<FormInstance>()
+
+// 网站表单数据
+const link = ref<Link>({
     title: "",
     description: "",
     email: "",
     image: "",
     url: "",
+    type: "",
 })
+
+const rules = {
+    title: [
+        { required: true, message: "网站标题不能为空", trigger: "blur" },
+        { min: 2, max: 30, message: "网站标题限制在2 ~ 30个字符", trigger: "blur" }
+    ],
+    description: [
+        { required: true, message: "网站描述不能为空", trigger: "blur" },
+        { min: 2, max: 100, message: "网站描述限制在2 ~ 100个字符", trigger: "blur" }
+    ],
+    email: [
+        { required: true, message: "网站邮箱不能为空", trigger: "blur" }
+    ],
+    image: [
+        { required: true, message: "网站图标不能为空", trigger: "blur" }
+    ],
+    url: [
+        { required: true, message: "网站链接不能为空", trigger: "blur" }
+    ],
+    type: [
+        { required: true, message: "网站类型不能为空", trigger: "blur" }
+    ]
+}
+
+// 提交表单
+const submit = () => {
+    // 新增之前先校验一下数据是否合法
+    form.value?.validate(async valid => {
+        if (valid) {
+            // 新增网站
+            await addLinkDataAPI(link.value)
+
+            list.value = {}
+
+            ElNotification({
+                title: '成功',
+                message: "🎉提交成功，请等待审核",
+                type: 'success',
+            })
+
+            // 重置校验并初始化数据
+            form.value?.resetFields()
+
+            // 获取最新数据
+            getLinkList()
+
+            // 关闭表单弹框
+            model.value = false
+        }
+    })
+}
 </script>
 
 <template>
     <div class="swiper">
         <Swiper src="https://bu.dusays.com/2023/11/05/65473848ed863.jpg" :Ripple="true">
             <div class="title">一个人的寂寞, 一群人的狂欢!</div>
-            <div class="application">申请友联</div>
+            <div class="application" @click="model = true">申请友联</div>
         </Swiper>
     </div>
 
     <!-- 朋友圈 -->
     <div class="bg">
         <div class="Friend" v-loading="loading" :element-loading-svg="svg" element-loading-svg-view-box="-10, -10, 50, 50">
-            <div class="cate" v-for="{ type, list }, index in linkData" :key="index">
-                <div class="title">{{ type }}</div>
+            <div class="cate" v-for="item, index in list" :key="index">
+                <div class="title">{{ item.type }}</div>
 
                 <div class="list">
-                    <div class="item" v-for="item in list">
-                        <a :href="item.url" target="_blank" class="box">
-                            <img :src="item.image" alt="">
+                    <div class="item" v-for="web in item.list">
+                        <a :href="web.url" target="_blank" class="box">
+                            <img :src="web.image" alt="">
 
                             <div class="info">
-                                <div class="name">{{ item.title }}</div>
-                                <div class="description">{{ item.description }}</div>
+                                <div class="name">{{ web.title }}</div>
+                                <div class="description">{{ web.description }}</div>
                             </div>
                         </a>
                     </div>
@@ -83,42 +150,50 @@ const linkForm = ref({
             </div>
 
             <!-- 空状态 -->
-            <!-- <Null style="margin-top: 30px;" v-if="!loading && !linkData?.length" /> -->
-            <!-- <Null style="margin-top: -40px;" v-if="!linkData?.length" /> -->
+            <Null v-if="!loading && list?.length" />
         </div>
     </div>
 
     <!-- 申请友联表单 -->
-    <el-dialog v-model="dialogVisible" title="申请友情链接" width="30%" class="linkForm">
-        <el-form label-position="top" label-width="100px" :model="linkForm" size="large" style="max-width: 460px">
-            <el-form-item label="标题">
-                <el-input v-model="linkForm.title" />
-            </el-form-item>
+    <div class="link">
+        <el-dialog v-model="model" title="申请友情链接" width="30%">
+            {{ link }}
+            <el-form ref="form" label-position="top" label-width="100px" :rules="rules" :model="link" size="large"
+                style="max-width: 460px">
+                <el-form-item label="标题" prop="title">
+                    <el-input v-model="link.title" placeholder="Thrive" />
+                </el-form-item>
 
-            <el-form-item label="描述">
-                <el-input v-model="linkForm.description" />
-            </el-form-item>
+                <el-form-item label="描述" prop="description">
+                    <el-input v-model="link.description" placeholder="记录前端、Python、Java点点滴滴" />
+                </el-form-item>
 
-            <el-form-item label="邮箱">
-                <el-input v-model="linkForm.email" />
-            </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="link.email" placeholder="3311118881@qq.com" />
+                </el-form-item>
 
-            <el-form-item label="图标">
-                <el-input v-model="linkForm.image" />
-            </el-form-item>
+                <el-form-item label="头像" prop="image">
+                    <el-input v-model="link.image" placeholder="https://liuyuyang.net/logo.png" />
+                </el-form-item>
 
-            <el-form-item label="链接">
-                <el-input v-model="linkForm.url" />
-            </el-form-item>
-        </el-form>
+                <el-form-item label="链接" prop="url">
+                    <el-input v-model="link.url" placeholder="https://liuyuyang.net/" />
+                </el-form-item>
 
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button @click="dialogVisible = false">取消</el-button>
-                <el-button type="primary" @click="dialogVisible = false">提交</el-button>
-            </span>
-        </template>
-    </el-dialog>
+                <el-form-item label="类型" prop="type">
+                    <el-select v-model="link.type" placeholder="请选择你的网站类型" style="width: 100%;">
+                        <el-option v-for="item in linkTypeList" :key="item.id" :label="item.name" :value="item.id" />
+                    </el-select>
+                </el-form-item>
+            </el-form>
+
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button type="primary" size="large" @click="submit">提交申请</el-button>
+                </span>
+            </template>
+        </el-dialog>
+    </div>
 </template>
 
 <style scoped lang="scss">
@@ -185,7 +260,7 @@ const linkForm = ref({
     width: $w;
     padding: 30px;
     margin: 50px auto;
-    background-color: $bgColor;
+    background-color: $bgColor2;
     @include container;
 
     .cate {
@@ -268,17 +343,33 @@ const linkForm = ref({
     }
 }
 
-.linkForm {
-    .el-dialog__body {
-        padding: 10px 20px;
+.link {
+    :deep(.el-dialog) {
+        .el-dialog__header {
+            text-align: center;
+        }
+
+        .el-dialog__body {
+            padding: 10px 30px;
+        }
+
+        .dialog-footer .el-button {
+            width: 100%;
+        }
     }
 
     .el-form-item {
         margin-bottom: 20px;
     }
-}
 
-.dialog-footer .el-button {
-    padding: 0 10px;
+    .el-form {
+        .el-form-item:last-child {
+            margin-bottom: 5px;
+        }
+    }
+
+    .el-select-dropdown__item{
+        padding: 0 32px 0 20px !important;
+    }
 }
 </style>
