6be00239c0f7cc4c31a5f246fd6819a4d7998f15
LiuYuYang01 <3311118881@qq.com>
2024-02-09 17:24:40 +0800

    优化样式文件结构

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import { createApp } from 'vue'
 import pinia from '@/stores'
 
 import 'element-plus/dist/index.css'
-// 公共样式
-import '@/styles/public.scss'
-// 页面自适应
-import '@/styles/reactive/index.scss'
-// 主题切换
-import '@/styles/theme.scss'
+import '@/styles/index.scss'
 
 import App from './App.vue'
 import router from '@/router'
